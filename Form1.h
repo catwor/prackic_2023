7310df016a8741549b94fb3924fb1b1302fe0a06
catwor <ivan-tetyx@yandex.ru>
2023-07-13 18:53:39 +0400

    feshion menu

--- Form1.h
+++ Form1.h
@@ -35,6 +35,47 @@ namespace prackic2023 {
 			}
 		}
 
+	protected:
+
+
+
+
+
+
+
+
+	private: System::Windows::Forms::Button^ button1;
+
+
+
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+
+	private: System::Windows::Forms::Button^ button5;
+
+	private: System::Windows::Forms::ListBox^ listBox1;
+
+
+
+
+	private: System::Windows::Forms::ListView^ listView1;
+	private: System::Windows::Forms::ListBox^ listBox2;
+	private: System::Windows::Forms::ListBox^ listBox3;
+	private: System::Windows::Forms::ListBox^ listBox4;
+	private: System::Windows::Forms::ListBox^ listBox5;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::TextBox^ textBox3;
+
+
+
+
+
+
+	protected:
+
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
@@ -48,18 +89,202 @@ namespace prackic2023 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->listView1 = (gcnew System::Windows::Forms::ListView());
+			this->listBox2 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox3 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox4 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox5 = (gcnew System::Windows::Forms::ListBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(474, 207);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(200, 60);
+			this->button1->TabIndex = 9;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(54, 32);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 52);
+			this->textBox1->TabIndex = 13;
+			this->textBox1->Text = L"N student";
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(54, 122);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(248, 50);
+			this->textBox2->TabIndex = 14;
+			this->textBox2->Text = L"Surename";
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(231, 34);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(200, 50);
+			this->button5->TabIndex = 16;
+			this->button5->Text = L"fix";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &Form1::button5_Click);
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 16;
+			this->listBox1->Location = System::Drawing::Point(54, 207);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(100, 36);
+			this->listBox1->TabIndex = 18;
+			// 
+			// listView1
+			// 
+			this->listView1->HideSelection = false;
+			this->listView1->Location = System::Drawing::Point(191, 207);
+			this->listView1->Name = L"listView1";
+			this->listView1->Size = System::Drawing::Size(240, 300);
+			this->listView1->TabIndex = 23;
+			this->listView1->UseCompatibleStateImageBehavior = false;
+			// 
+			// listBox2
+			// 
+			this->listBox2->FormattingEnabled = true;
+			this->listBox2->ItemHeight = 16;
+			this->listBox2->Location = System::Drawing::Point(54, 271);
+			this->listBox2->Name = L"listBox2";
+			this->listBox2->Size = System::Drawing::Size(100, 36);
+			this->listBox2->TabIndex = 24;
+			// 
+			// listBox3
+			// 
+			this->listBox3->FormattingEnabled = true;
+			this->listBox3->ItemHeight = 16;
+			this->listBox3->Location = System::Drawing::Point(54, 339);
+			this->listBox3->Name = L"listBox3";
+			this->listBox3->Size = System::Drawing::Size(100, 36);
+			this->listBox3->TabIndex = 25;
+			// 
+			// listBox4
+			// 
+			this->listBox4->FormattingEnabled = true;
+			this->listBox4->ItemHeight = 16;
+			this->listBox4->Location = System::Drawing::Point(54, 407);
+			this->listBox4->Name = L"listBox4";
+			this->listBox4->Size = System::Drawing::Size(100, 36);
+			this->listBox4->TabIndex = 26;
+			// 
+			// listBox5
+			// 
+			this->listBox5->FormattingEnabled = true;
+			this->listBox5->ItemHeight = 16;
+			this->listBox5->Location = System::Drawing::Point(54, 470);
+			this->listBox5->Name = L"listBox5";
+			this->listBox5->Size = System::Drawing::Size(100, 36);
+			this->listBox5->TabIndex = 27;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(474, 288);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(200, 60);
+			this->button2->TabIndex = 28;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(474, 369);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(200, 60);
+			this->button3->TabIndex = 29;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(474, 446);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(200, 60);
+			this->button4->TabIndex = 30;
+			this->button4->Text = L"button4";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(474, 32);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(200, 50);
+			this->button6->TabIndex = 31;
+			this->button6->Text = L"fix";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(379, 122);
+			this->textBox3->Multiline = true;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(295, 50);
+			this->textBox3->TabIndex = 32;
+			this->textBox3->Text = L"Surename";
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(887, 503);
+			this->ClientSize = System::Drawing::Size(708, 547);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->listBox5);
+			this->Controls->Add(this->listBox4);
+			this->Controls->Add(this->listBox3);
+			this->Controls->Add(this->listBox2);
+			this->Controls->Add(this->listView1);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+
+private: System::Void menuStrip2_ItemClicked(System::Object^ sender, System::Windows::Forms::ToolStripItemClickedEventArgs^ e) {
+}
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
